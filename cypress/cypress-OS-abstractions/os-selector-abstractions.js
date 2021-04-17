a78d2fde2455a11f18916606b373f7ac161adaa7
João Rocha <joaomiguelrocha@gmail.com>
2021-04-17 13:18:30 +0100

    cleaning up for new project setup

--- cypress/cypress-OS-abstractions/os-selector-abstractions.js
+++ cypress/cypress-OS-abstractions/os-selector-abstractions.js
@@ -10,19 +10,6 @@ export function closeEyes(){
     cy.eyesClose()
 }
 
-// ######## iFrame ABSTRACTION #########################################################
-
-function getIframeDocument (src) {
-    return cy
-    .get('iframe[src*='+src+']')
-    .its('0.contentDocument.body').should('not.be.empty')
-  }
-  
-  export function getIframeBody(src) {
-    return getIframeDocument(src)
-    .then(cy.wrap)
-  }
-
 // ######## Traditional WEB Setup ABSTRACTION ###########################################
 
 export function ensureTraditionalWebRequiredCookiesEnabled(){
@@ -54,3 +41,39 @@ export function getByElementOsIdAndIndexWithAnchor(anchorOsId, htmlElem, osId, i
     return cy.get(`*[Id$=${anchorOsId}] ${htmlElem}[Id$=${osId}]:nth-child${index + 1}`)
 }
 
+// ######## iFrame ABSTRACTION #########################################################
+
+function getIframeDocument (src) {
+    return cy
+    .get('iframe[src*='+src+']')
+    .its('0.contentDocument.body').should('not.be.empty')
+  }
+  
+function getIframeBody(src) {
+    return getIframeDocument(src)
+    .then(cy.wrap)
+}
+
+export function getByOsId_inIframe(iframeScreen, osId) {
+    return getIframeBody(iframeScreen).find(`*[Id$=${osId}]`)
+ }
+ 
+ export function getByElementAndOsId_inIframe(iframeScreen, htmlElem, osId) {
+     return getIframeBody(iframeScreen).find(`${htmlElem}[Id$=${osId}]`)
+ }
+ 
+ export function getByElementOsIdAndIndex_inIframe(iframeScreen, htmlElem, osId, index) {
+     return getIframeBody(iframeScreen).find(`${htmlElem}[Id$=${osId}]:nth-child${index + 1}`)
+ }
+ 
+ export function getByOsIdWithAnchor_inIframe(iframeScreen, anchorOsId, osId) {
+     return getIframeBody(iframeScreen).find(`*[Id$=${anchorOsId}] *[Id$=${osId}]`)
+ }
+ 
+ export function getByElementAndOsIdWithAnchor_inIframe(iframeScreen, anchorOsId, htmlElem, osId) {
+     return getIframeBody(iframeScreen).find(`*[Id$=${anchorOsId}] ${htmlElem}[Id$=${osId}]`)
+ }
+ 
+ export function getByElementOsIdAndIndexWithAnchor_inIframe(iframeScreen, anchorOsId, htmlElem, osId, index) {
+     return getIframeBody(iframeScreen).find(`*[Id$=${anchorOsId}] ${htmlElem}[Id$=${osId}]:nth-child${index + 1}`)
+ }
