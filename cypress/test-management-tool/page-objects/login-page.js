07eeb215de1d787a7405012fca84d6dc46cacd42
J. Rocha <joaomiguelrocha@gmail.com>
2021-07-22 01:17:31 +0100

    login feature tests complete

--- cypress/test-management-tool/page-objects/login-page.js
+++ cypress/test-management-tool/page-objects/login-page.js
@@ -4,10 +4,16 @@ const usernameInput = "*[data-testid=usernameInput]"
 const passwordInput = "*[data-testid=passwordInput]"
 const rememberMe = "*[data-testid=rememberMe]"
 const LoginBtn = "*[data-testid=loginBtn]"
+const usernameRequiredSpan = "*[data-testid=usernameInput] ~.validation-message"
+const passwordRequiredSpan = "*[data-testid=passwordInput] ~.validation-message"
 
 export function fillCredentials(username, password, clickRememberme){
-    cy.get(usernameInput).type(username)
-    cy.get(passwordInput).type(password)
+    if(username != ""){
+        cy.get(usernameInput).type(username)
+    }
+    if(password != ""){
+        cy.get(passwordInput).type(password)
+    }
     if(clickRememberme = true)
     {
         cy.get(rememberMe).click()
@@ -17,9 +23,33 @@ export function fillCredentials(username, password, clickRememberme){
 export function clearForm() {
     cy.get(usernameInput).clear()
     cy.get(passwordInput).clear()
-    cy.get(rememberMe).clear()
+    cy.get(rememberMe).uncheck()
 }
 
 export function clickLogin() {
     cy.get(LoginBtn).click()
+}
+
+export function usernameRequiredIsVisible(expectation) {
+    if(expectation = true)
+    {
+        cy.get(usernameRequiredSpan).should('be.visible')
+        cy.get(usernameRequiredSpan).should("have.text", "Required field!")
+    }
+    else
+    {
+        cy.get(usernameRequiredSpan).should('not.be.be.visible')
+    }
+}
+
+export function passwordRequiredIsVisible(expectation) {
+    if(expectation = true)
+    {
+        cy.get(passwordRequiredSpan).should('be.visible')
+        cy.get(passwordRequiredSpan).should("have.text", "Required field!")
+    }
+    else
+    {
+        cy.get(passwordRequiredSpan).should('not.be.be.visible')
+    }
 }
